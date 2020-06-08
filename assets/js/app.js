d436dff4a2db7a8094f6692f622b45248f51f9fc
StevenGrl <steven.grillon.45@gmail.com>
2020-06-08 14:24:47 +0200

    import bootstrap and scss works

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,10 +6,12 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.css';
+// import '../css/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+import $ from 'jquery';
+
+import 'bootstrap/dist/js/bootstrap'
 
 $(document).ready(function () {
     console.log('$ okay')
